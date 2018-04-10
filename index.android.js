ca9f8acdd7175319b9e0c5d7dadd055dcf82b63c
s48024 <s48024@beuth-hochschule.de>
2018-04-11 00:26:23 +0200

    build not working

--- index.android.js
+++ index.android.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import ListSelectScreen from './screens/ListSelectScreen';
 import ListCreateScreen from './screens/ListCreateScreen';
+import ListDetailsScreen from './screens/ListDetailsScreen';
 import TilesScreen from './screens/TilesScreen';
 
 const Navigation = StackNavigator({
   ListSelect: {screen: ListSelectScreen},
   ListCreate: {screen: ListCreateScreen},
+  Details: {screen: ListDetailsScreen},
   Tiles: {screen: TilesScreen},
 }, {
   initialRouteName: 'ListSelect',
